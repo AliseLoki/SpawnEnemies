ceca3fda8698d96ca0a97bccee0a2648ebc2f05a
AliseLoki <AliseLoki777@yandex.ru>
2023-10-06 13:41:44 +0300

    change

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private GameObject _target;
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        if (_target != null)
+            transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
+    }
+
+    public void Initialize(GameObject target)
+    {
+        _target = target;
     }
 }
